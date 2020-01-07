69d5b3ef76e458d140878db53c68b401ba8ebc60
zhjch321123 <48094072+zhjch321123@users.noreply.github.com>
2020-01-07 10:05:37 -1000

    Add files via upload

---
+++ Figure_4B/fig_4b.r
@@ -0,0 +1,4 @@
+library(ggplot2)
+df <- read.table("123.txt",header=T,row.names=1,sep="\t")
+g3=ggplot(df)+geom_smooth(aes(x=x,y=y, colour=colour),method='auto',alpha=.3, size=3)+scale_colour_brewer(type = "div", palette = "Paired")+geom_point(data=df, aes(x=x,y=y, colour=colour), alpha=.5, size=1)
+plot(g3)+theme_minimal()
