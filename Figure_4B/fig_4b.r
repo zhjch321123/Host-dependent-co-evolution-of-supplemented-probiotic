ec30515f7bf18e9e94cd16b081e1e2d312208229
zhjch321123 <48094072+zhjch321123@users.noreply.github.com>
2020-01-07 12:39:14 -0800

    Update fig_4b.r

--- Figure_4B/fig_4b.r
+++ Figure_4B/fig_4b.r
@@ -1,4 +1,4 @@
 library(ggplot2)
-df <- read.table("123.txt",header=T,row.names=1,sep="\t")
+df <- read.table("figure_4b.txt",header=T,row.names=1,sep="\t")
 g3=ggplot(df)+geom_smooth(aes(x=x,y=y, colour=colour),method='auto',alpha=.3, size=3)+scale_colour_brewer(type = "div", palette = "Paired")+geom_point(data=df, aes(x=x,y=y, colour=colour), alpha=.5, size=1)
 plot(g3)+theme_minimal()
